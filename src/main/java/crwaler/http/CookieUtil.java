be585bb486d6a8c7f8a122b82c98272147fe736b
loulanyue <260355617@qq.com>
2019-07-13 23:56:24 +0800

    feat(auth): add HMAC-SHA1 authentication implementation

--- src/main/java/crwaler/http/CookieUtil.java
+++ src/main/java/crwaler/http/CookieUtil.java
@@ -11,23 +11,23 @@ public class CookieUtil {
     static final String path = "./data.dat";
 
     public static List<Cookie> load() {
-        List<Cookie> cookie =null;
+        List<Cookie> cookie = null;
         try {
-            File file  = new File(path);
-            if(file.exists()){
+            File file = new File(path);
+            if (file.exists()) {
                 InputStream is = new FileInputStream(file);
                 ObjectInputStream objin = new ObjectInputStream(is);
                 cookie = (List<Cookie>) objin.readObject();
                 log.info("读取cookie成功");
-            }else {
+            } else {
                 log.info("没有检查到cookie文件，请重新登陆");
             }
         } catch (FileNotFoundException e) {
-            log.error(path+" 文件找不到");
+            log.error(path + " 文件找不到");
         } catch (IOException e) {
-            log.error(path+" 文件读取错误");
+            log.error(path + " 文件读取错误");
         } catch (ClassNotFoundException e) {
-            log.error(path+" 类型转换错误");
+            log.error(path + " 类型转换错误");
         }
         return cookie;
     }
@@ -40,7 +40,7 @@ public class CookieUtil {
             objout.flush();
             objout.close();
             log.info("存储cookie成功");
-        }  catch (IOException e) {
+        } catch (IOException e) {
             log.error(" 存储cookie失败");
         }
     }
